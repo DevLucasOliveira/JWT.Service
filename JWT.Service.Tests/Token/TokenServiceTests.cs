1ebf9e9311f83f7338569129af97b37a2325e7f2
DevLucasOliveira <lucasoliveira.si@outlook.com>
2020-11-12 21:52:16 -0300

    Refactoring tests

--- JWT.Service.Tests/Token/TokenServiceTests.cs
+++ JWT.Service.Tests/Token/TokenServiceTests.cs
@@ -6,15 +6,21 @@ namespace JWT.Service.Tests.Token
 {
     public class TokenServiceTests
     {
+        private readonly string _userId;
+        private readonly string _secret;
+        private readonly double _expiredToken;
+
+        public TokenServiceTests()
+        {
+            _userId = new int().ToString();
+            _secret = Guid.NewGuid().ToString();
+            _expiredToken = 6;
+        }
 
         [Fact]
         public void GenerateToken_ReturnsToken()
         {
-            var userId = new int().ToString();
-            var secret = Guid.NewGuid().ToString();
-            var expiredToken = 6;
-
-            var token = TokenService.GenerateToken(userId, secret, expiredToken);
+            var token = TokenService.GenerateToken(_userId, _secret, _expiredToken);
 
             Assert.NotNull(token);
             Assert.NotEmpty(token);
@@ -23,10 +29,7 @@ namespace JWT.Service.Tests.Token
         [Fact]
         public void GenerateToken_WhenSecretIsNull_ReturnsException()
         {
-            var userId = new int().ToString();
-            var expiredToken = 6;
-
-            var ex = TokenService.GenerateToken(userId, null, expiredToken);
+            var ex = TokenService.GenerateToken(_userId, null, _expiredToken);
 
             Assert.Equal("String reference not set to an instance of a String. (Parameter 's')", ex);
         }
@@ -34,10 +37,7 @@ namespace JWT.Service.Tests.Token
         [Fact]
         public void GenerateToken_WhenUserIdIsNull_ReturnsException()
         {
-            var secret = Guid.NewGuid().ToString();
-            var expiredToken = 6;
-
-            var ex = TokenService.GenerateToken(null, secret, expiredToken);
+            var ex = TokenService.GenerateToken(null, _secret, _expiredToken);
 
             Assert.Equal("Value cannot be null. (Parameter 'value')", ex);
         }
@@ -45,11 +45,7 @@ namespace JWT.Service.Tests.Token
         [Fact]
         public void GenerateToken_WhenExpiredTokenIsNull_ReturnsException()
         {
-            var userId = new int().ToString();
-            var secret = Guid.NewGuid().ToString();
-            var expiredToken = 0;
-
-            var ex = TokenService.GenerateToken(userId, secret, expiredToken);
+            var ex = TokenService.GenerateToken(_userId, _secret, 0);
 
             Assert.Equal("IDX12401: Expires: '[PII is hidden]' must be after NotBefore: '[PII is hidden]'.", ex);
         }
